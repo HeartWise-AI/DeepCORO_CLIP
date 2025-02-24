d53eabe4db8377331f6bf6b5f70b8bf476c4af71
JacquesDelfrate <jdelfrate504@gmail.com>
2025-02-25 01:24:54 +0500

    remove useless file in config/clip

--- config/clip/runner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# runner.sh
-# Activate environment
-python3 -m venv .venv
-source .venv/bin/activate  # On Linux/Mac
-
-# Execute torchrun with single GPU/node
-torchrun --standalone --nnodes=1 --nproc_per_node=1 "$1" --config "$2" "${@:3}"

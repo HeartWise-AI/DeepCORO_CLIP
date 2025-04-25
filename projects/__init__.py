25f84b6072a24990e794f234dff02a50b7e7dc21
Sarra Harrabi <harrabisarra2@gmail.com>
2025-04-25 21:57:27 +0000

    Ajout du class MultiviewProject

--- projects/__init__.py
+++ projects/__init__.py
@@ -1,5 +1,6 @@
 from projects.base_project import BaseProject
 from projects.linear_probing_project import LinearProbingProject
 from projects.contrastive_pretraining_project import ContrastivePretrainingProject
+from projects.deep_coro_multiview_project import MultiviewProject
 
-__all__ = ["ContrastivePretrainingProject", "LinearProbingProject", "BaseProject"]
+__all__ = ["ContrastivePretrainingProject", "LinearProbingProject", "BaseProject", "MultiviewProject"]

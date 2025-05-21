5f36ae96936672b514b3856111a3f8670464e30c
JacquesDelfrate <jdelfrate504@gmail.com>
2025-05-21 21:46:51 +0000

    video_dataset not deprecated and still used in RAG inference

--- dataloaders/__init__.py
+++ dataloaders/__init__.py
@@ -1,12 +1,14 @@
-# NOTE: VideoDataset is deprecated and should not be imported from this module. Use VideoClipDataset instead.
 from dataloaders.simple_text_dataset import SimpleTextDataset
-from dataloaders.video_clip_dataset import VideoClipDataset, custom_collate_fn
 from dataloaders.stats_dataset import StatsDataset, stats_collate_fn
+from dataloaders.video_clip_dataset import VideoClipDataset, custom_collate_fn
+from dataloaders.video_dataset import VideoDataset, custom_collate_fn as video_collate_fn
 
 __all__ = [
     "SimpleTextDataset",
     "VideoClipDataset",
+    "VideoDataset",
     "StatsDataset",
     "custom_collate_fn",
     "stats_collate_fn",
+    "video_collate_fn",
 ]
--- utils/generate_video_embeddings.py
+++ utils/generate_video_embeddings.py
@@ -6,8 +6,7 @@ from typing import Any
 from torch.utils.data import DataLoader
 
 from models.video_encoder import VideoEncoder
-# DEPRECATED: Use VideoClipDataset instead of VideoDataset. This will be removed in a future release.
-from dataloaders.video_dataset import VideoDataset, custom_collate_fn  # Deprecated, use VideoClipDataset
+from dataloaders.video_dataset import VideoDataset, custom_collate_fn
 
 import argparse
 
@@ -34,7 +33,6 @@ def load_checkpoint(
     return torch.load(checkpoint_path, map_location='cpu', weights_only=True)
 
 def generate_video_embeddings():
-    # DEPRECATED: The following instantiation uses VideoDataset, which is deprecated. Use VideoClipDataset instead.
     video_dataset: VideoDataset = VideoDataset(
         data_filename='data/CathEF_MHI_UCSF_2016-to-july-2022-and-2023-08-30-post-CathEF_alpha.csv',
         split=split,
